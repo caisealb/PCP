001c4b8b905310eece4bb71aaaac401b7f7f6ab7
caisealb <caiseal.r.beardow@bath.edu>
2019-09-16 11:59:37 +0200

    Updated for first test

--- Arduino/FSR_Example/FSR_Example.ino
+++ Arduino/FSR_Example/FSR_Example.ino
@@ -1,9 +1,61 @@
-void setup() {
-  // put your setup code here, to run once:
+//Variables
+#define fsrPin A0
 
+int sensorValue = -10000;
+int prevSensorValue = -10000;
+int deviation = 0;
+double voltageValue;
+double newtonValue;
+
+
+void setup() {
+ Serial.begin(9600);
+ pinMode(sensorValue, INPUT);
+
+ deviation = 10;
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+  double convertToNewtons(double voltage){
+  /*General fitting model: 
+       f(x) = a*exp(b*x) + c*exp(d*x)
+     Coefficients (with 95% confidence bounds):
+       a =     0.01419  (0.01163, 0.01676)
+       b =      0.9523  (0.8922, 1.012)
+       c =    -0.01461  (-0.02317, -0.006043)
+       d =      -2.231  (-6.605, 2.142)
+       Goodness of fit:
+       SSE: 7.906e-06
+       R-square: 0.9999
+       Adjusted R-square: 0.9997
+       RMSE: 0.001988
+  */
+  
+  double a = 0.01419;
+  double b = 0.9523;
+  double c = -0.01461;
+  double d = -2.231;
 
+  return((a*exp(b*voltage) + (c*exp(d*voltage))*9.81)); //9.81 multiplication converts KgF to Newtons
+  }
+
+
+void loop() {
+ delay(100); //Corresponds to 10Hz sample rate
+ sensorValue = analogRead(fsrPin);
+ if(sensorValue >= (prevSensorValue - deviation) && sensorValue <= (prevSensorValue + deviation)){
+  return;
+
+  voltageValue = double((sensorValue*5))/1023; //convert to voltage
+  newtonValue = convertToNewtons(voltageValue); //actual force value
+
+  Serial.print("Pressure: ");
+  Serial.print(sensorValue);
+  Serial.print(" ()-1023) steps, ");
+  Serial.print(voltageValue);
+  Serial.print(" (v), ");
+  Serial.print(newtonValue);
+  Serial.println(" N.");
+
+  prevSensorValue = sensorValue;
+ }
 }
