00b425185b2377360db35b0b5979e4404ba8e963
caisealb <caiseal.r.beardow@bath.edu>
2019-09-16 13:53:33 +0200

    Updated - print(my_thing.to_json()) doesn't work so commented out

--- random-data.py
+++ random-data.py
@@ -8,9 +8,9 @@ load_dotenv()
 THING_ID = os.environ['THING_ID']
 THING_TOKEN = os.environ['THING_TOKEN']
 
-my_thing = Thing(thing_ID=THING_ID, token=THING_TOKEN)
+my_thing = Thing(thing_id=THING_ID, token=THING_TOKEN)
 my_thing.read()
-print(my_thing.to_json())
+# print(my_thing.to_json())
 
-my_property = my_thing.find_or_create_property("My Random Property", PropertyType.THREE_DIMENSIONs)
+my_property = my_thing.find_or_create_property("My Random Property", PropertyType.THREE_DIMENSIONS)
 print(my_property.to_json())
