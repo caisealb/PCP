271ce4fbfafe82dd40f24ff7031e45507bb56e08
caisealb <caiseal.r.beardow@bath.edu>
2019-09-27 15:17:42 +0200

    New arduino file for receiving distance and visualising with LEDs

---
+++ Arduino/BNO/BNO_PI_TO_LED/LED_FUNCTIONS.ino
@@ -0,0 +1,45 @@
+void distCounterLED() {
+  illumNoCounter();
+
+  //Serial.println(illumNoRound);
+
+  if ((illumNoRound > 0) && (illumNoRound <= 16)) {
+    for (int i = 0; i < illumNoRound; i++) {
+      ring1.setPixelColor(i, red, green, blue);
+      ring1.show();
+    }
+    if ((illumNoRound > 0) && (illumNoRound <= 4)) {
+      red = 255;
+      green = 0;
+      blue = 0;
+    }
+    else if ((illumNoRound > 4) && (illumNoRound <= 8)) {
+      red = 255;
+      green = 128;
+      blue = 0;
+    }
+    else if ((illumNoRound > 8) && (illumNoRound <= 12)) {
+      red = 255;
+      green = 255;
+      blue = 0;
+    }
+    else if ((illumNoRound > 12) && (illumNoRound < 16)) {
+      red = 128;
+      green = 255;
+      blue = 0;
+    }
+  }
+
+  else {
+    for (int i = 0; i < ring1.numPixels(); i++) {
+      ring1.setPixelColor(i, 0, 0, 0);
+      ring1.show();
+    }
+  }
+}
+
+void illumNoCounter() {
+  illumNo = (totalDistance / 10);
+  illumNoRound = illumNo;
+  illumNoRound = illumNoRound % 17;
+}
