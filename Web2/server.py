df867685c6169a448270299cbabacb6a26325c63
kikideurvorst <kikideurvorst@hotmail.com>
2019-09-30 14:35:10 +0200

    testing with distance meter

--- Web2/server.py
+++ Web2/server.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/distance')
+def distance():
+    return render_template('distance.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
