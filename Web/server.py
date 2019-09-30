616775b3c4f1a3369f0d6625686125e35a261d58
caisealb <caiseal.r.beardow@bath.edu>
2019-09-30 12:30:21 +0200

    Simple web app with /home and /gauge; button msg not functional

---
+++ Web/server.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, render_template
+from flask_socketio import SocketIO, emit
+app = Flask(__name__)
+
+app.config['SECRET KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/home')
+def home():
+    return render_template('index.html')
+
+@app.route('/gauge')
+def gauge():
+    return render_template('gauge.html')
+
+@app.route('/api/sensors', methods = ['GET'])
+def list():
+    return str(sensors)
+
+@app.route('/api/sensors/<path:sensor_id>', methods = ['GET'])
+def read(sensor_id):
+    global sensors
+    return sensors[sensor_id]
+
+@app.route('/api/sensors', methods = ['POST'])
+def create():
+    sensors.append(request.json["sensorName"])
+    return 'Added sensor!'
+
+
+if __name__ == '__main__':
+    socketio.run(app, host = '0.0.0.0')
