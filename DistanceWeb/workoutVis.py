bae58a5eea3c11cdb6a9f2c5b1b9daef7bfc295d
caisealb <caiseal.r.beardow@bath.edu>
2019-10-14 14:49:07 +0200

    removed flushInput (debug)

--- DistanceWeb/workoutVis.py
+++ DistanceWeb/workoutVis.py
@@ -138,7 +138,6 @@ signal.signal(signal.SIGINT, keyboard_interrupt_handler)
 #Run serial comms
 def serialComms():
     s1 = serial.Serial(port, 9600)
-    s1.flushInput()
     try:
         s1.open()
     except:
