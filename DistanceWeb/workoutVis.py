bc22bd414afb735a4e86c12c6c496a51af4af458
caisealb <caiseal.r.beardow@bath.edu>
2019-10-21 11:58:40 +0200

    try for loop

--- DistanceWeb/workoutVis.py
+++ DistanceWeb/workoutVis.py
@@ -172,13 +172,19 @@ def serialComms():
                 ser.read()
                 serData = (ser.readline().decode())
                 print("Reading data...")
+
                 # print(serData)
                 dataElements = [x.strip() for x in serData.split(',')]
                 # print(dataElements)
-                latitude = dataElements[1]
-                longitude = dataElements[3]
-                print(latitude)
-                print(longitude)
+                for x in dataElements:
+                    if (x == "lat:"):
+                        print(x+1)
+                    else if (x == "lng:"):
+                        print(x+1)
+                # latitude = dataElements[1]
+                # longitude = dataElements[3]
+                # print(latitude)
+                # print(longitude)
             except:
                 print("Unexpected error:", sys.exc_info()[0])
                 raise
