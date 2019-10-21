dc0f5f29b2087326b68a8061832658607b62c24f
caisealb <caiseal.r.beardow@bath.edu>
2019-10-21 16:42:59 +0200

    removed itemgetter

--- DistanceWeb/workoutVis.py
+++ DistanceWeb/workoutVis.py
@@ -188,7 +188,7 @@ def serialComms():
                 print(dataElements)
                 # print(dataElements)
                 latitudes = dataElements[1::5]
-                latFloat = itemgetter(0)(latitudes)
+                latFloat = [float(i) for i in latitudes]
                 print("Latitude:")
                 print(latFloat)
                 longitudes = dataElements[3::5]
