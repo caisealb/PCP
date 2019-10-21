1cbcb7f1fe76a0eb7d4c847379e3f348059581b8
caisealb <caiseal.r.beardow@bath.edu>
2019-10-21 16:48:38 +0200

    debug msg

--- DistanceWeb/workoutVis.py
+++ DistanceWeb/workoutVis.py
@@ -186,9 +186,10 @@ def serialComms():
                 # print(serData)
                 dataElements = [x.strip() for x in serData.split(',')]
                 print(dataElements)
+                print(dataElements[1])
                 # print(dataElements)
                 latitudes = dataElements[1::5]
-                latFloat = (float(latitudes))
+                latFloat = ([float(i) for i in latitudes])
                 print("Latitude:")
                 print(latFloat)
                 longitudes = dataElements[3::5]
